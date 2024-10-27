64c0cd6d6a3e9b4cdd0a627d6646782b095aa478
Sanyam <sanyam@leena.ai>
2024-10-27 18:47:11 +0530

    feature/auth: added auth module and packages

---
+++ src/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule, // Ensure ConfigModule is available
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI')
+      })
+    })
+  ],
+  exports: [MongooseModule]
+})
+export class DatabaseModule {}
